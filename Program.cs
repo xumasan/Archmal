c1b282b0619a477cc3aa7ebeb4e633850e5cd593
kumasan1011 <t16318hk@sfc.keio.ac.jp>
2018-11-26 23:33:14 +0900

    commandを実装

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿#define LEARN
-
-using System;
+﻿using System;
 
 namespace Archmal
 {
@@ -8,24 +6,29 @@ namespace Archmal
     {
         static void Main(string[] args)
         {
-#if LEARN
-            Eval.Init();
-            new Learn().LearnAll(3, 10);
-            Position pos = new Position();
-            Search.Iteration(pos);
-#else
-            Int32 port;
-            Int32.TryParse(args[1], out port);
+            Console.WriteLine("Usage: command [command options]");
+			Console.WriteLine("command:");
+            Console.WriteLine("\tclient    -- client-mode");
+            Console.WriteLine("\tlearn  -- [learnDepth][iterate] Bonanza Method");
 
             Eval.Init();
 
-            while(true)
+            var sub = Console.ReadLine().Split(' ');
+            if (sub[0] == "lean")
             {
+                new Learn().LearnAll(int.Parse(sub[1]), int.Parse(sub[2]));
                 Position pos = new Position();
-                Client client = new Client(pos);
-                client.Start(args[0], port);
+                Search.Iteration(pos);
             }
-#endif
+            else if (sub[0] == "client")
+            {
+                while(true)
+                {
+                    Position pos = new Position();
+                    Client client = new Client(pos);
+                    client.Start(args[0], Int32.Parse(sub[1]));
+                }
+            }
         }
     }
 }
