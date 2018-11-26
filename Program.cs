d199d4014b7c0c2b79c70525688272a6bbbe5d52
kumasan1011 <t16318hk@sfc.keio.ac.jp>
2018-11-26 21:27:59 +0900

    静止探索を実装

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace Archmal
 #if LEARN
             Eval.Init();
             new Learn().LearnAll(3, 10);
-            //Position pos = new Position();
-            //Search.Iteration(pos);
+            Position pos = new Position();
+            Search.Iteration(pos);
 #else
             Int32 port;
             Int32.TryParse(args[1], out port);
--- Search.cs
+++ Search.cs
@@ -22,11 +22,75 @@ namespace Archmal
 
             if (depth <= 0)
             {
-                return Eval.Evaluate_kkpp(pos);
+                return QSearch(pos, alpha, beta, 0, ply, pvNode, PV);
             }
 
             GenerateMoves gm = new GenerateMoves(pos);
             List<Move> moves = gm.GenerateAllMoves();
+
+            moveCount = 0;
+
+            foreach (Move m in moves)
+            {
+                List<Move> newPV = null;
+
+                int newDepth = depth - 1;
+                bool win = pos.DoMove(m);
+
+                moveCount++;
+
+                if (win)
+                {
+                    pos.UndoMove(m);
+                    PV.Add(m);
+                    return Value.EvalInfinite;
+                }
+                
+                value = -FullSearch(pos, -beta, -alpha, newDepth, newPly, true, newPV = new List<Move>{m});
+
+                pos.UndoMove(m);
+
+                if (value > bestValue)
+                {
+                    bestValue = value;
+                    bestPV = newPV;
+
+                    if (value > alpha)
+                    {
+                        if (pvNode && value < beta) // alpha の更新
+                            alpha = value;
+                        else
+                        {
+                            // beta cut
+                            break;
+                        }
+                    }
+                }
+            }
+
+            PV.AddRange(bestPV);
+            return bestValue;
+        }
+
+        public static int QSearch(Position pos, int alpha, int beta, int depth, int ply, bool pvNode, List<Move> PV)
+        {
+            bool rootNode = pvNode && ply == 0;
+            int value = 0;
+            int moveCount;
+            int newPly = ply + 1;
+            var bestPV = new List<Move>();
+
+            int standPat = Eval.Evaluate_kkpp(pos);
+			int bestValue = standPat;
+			
+            if (bestValue >= beta) 
+                return bestValue; // standPat
+			
+            if (depth <= -6) 
+                return bestValue; // limit
+
+            GenerateMoves gm = new GenerateMoves(pos);
+            List<Move> moves = gm.GenerateCaptureMoves();
 
             moveCount = 0;
 
